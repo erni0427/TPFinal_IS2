f8183fe187681478f155c63c335224b6d1ef26d2
erni0427 <esteban.erni@ingenieria.uner.edu.ar>
2024-10-16 13:33:02 -0300

    Incorporacion de modulo parametros que detalla los tipos de especies de pastos y los tipos de escenarios climaticos

---
+++ modules/parametros.py
@@ -0,0 +1,9 @@
+species_params = {
+    "Cymodocea nodosa": {"growth_rate": 0.05, "mortality_rate": 0.02, "competitiveness": 3},
+    "Posidonia oceanica": {"growth_rate": 0.08, "mortality_rate": 0.03, "competitiveness": 2},
+    "Halophila stipulacea": {"growth_rate": 0.12, "mortality_rate": 0.04, "competitiveness": 10}
+}
+climate_scenarios = {
+    "RCP 2.6": {"temp_increase": 1.0, "salinity_increase": 0.1},
+    "RCP 8.5": {"temp_increase": 3.5, "salinity_increase": 0.4}
+}
